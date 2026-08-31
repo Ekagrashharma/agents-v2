802c526a09856b3c4d93943c8b9361f6fd1dcc33
ekagrashharma <ekagrashharma@gmail.com>
2026-08-31 16:14:05 +0000

    add dateTime tool call

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -2,7 +2,8 @@ import "dotenv/config";
 import { generateText, type ModelMessage } from "ai";
 import { GoogleGenAI } from "@google/genai";
 import { google } from "@ai-sdk/google"
-
+import { tools } from "./tools/index.ts"
+import { executeTool } from "./executeTools.ts";
 import { SYSTEM_PROMPT } from "./system/prompt.ts";
 
 import type { AgentCallbacks } from "../types.ts";
@@ -15,13 +16,18 @@ export async function runAgent(
   conversationHistory: ModelMessage[],
   callbacks: AgentCallbacks,
 ): Promise<any> {
-  const { text } = await generateText({
+  const { text , toolCalls } = await generateText({
     model: google(MODEL_NAME),
     prompt: userMessage,
     system: SYSTEM_PROMPT,
+    tools
   });
 
-  console.log(text);
+  console.log(text , toolCalls);
+
+  toolCalls.forEach(async(tc)=>{
+    console.log( await executeTool(tc.toolName , tc.input));
+  })
 }
 
-runAgent("hello my name is ekagra");
+runAgent("hello my name is ekagra , what is the current date and time ");
